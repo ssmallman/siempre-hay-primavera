b084486dd0bf5774e6f6e3d1da882af78eabedf2
Stephanie Smallman <>
2018-08-10 09:34:47 -0700

    Made a common page that fetches contents via url params

--- content/blog.js
+++ content/blog.js
@@ -3,3 +3,15 @@ function getContent(filename) {
 	return fetch("/" + filename)
 		.then(response => response.text())
 }
+
+function showContent(filename) {
+	getContent(filename).then(
+		content => {
+			const contentNode = document.getElementById('content');
+			contentNode.innerHTML = content;
+		},
+		error => {
+			console.error(error);
+		}
+	);
+}
