530aa27b53fe910cb5f7cabf1b448f873f8781e9
Stephanie Smallman <>
2018-11-06 18:09:56 -0800

    Add error handling for unrecognized poem names

--- content/blog.js
+++ content/blog.js
@@ -1,7 +1,16 @@
 // Returns a Promise<String>
 function getContent(filename) {
-	return fetch("/" + filename)
-		.then(response => response.text())
+	return fetch("/" + filename).then(
+		response => {
+			if (response.ok) {
+				return response.text();
+			} else {
+				const error = new Error("Got a non-successful response.");
+				error.response = response;
+				throw error;
+			}
+		}
+	);
 }
 
 function showContent(filename) {
@@ -11,7 +20,13 @@ function showContent(filename) {
 			contentNode.innerHTML = content;
 		},
 		error => {
+			const contentNode = document.getElementById('content');
+			contentNode.innerHTML = "Sorry, we couldn't find what you were looking for!";
+
 			console.error(error);
+			if (error.response) {
+				console.error("Response", error.response);
+			}
 		}
 	);
 }
